cb73c382333556065165b04f7887d85266b129b4
Alexander Hilmar Arnarson <alexander.hilmar@gmail.com>
2014-10-29 01:19:59 +0000

    huge commit

--- js/loader.js
+++ js/loader.js
@@ -1,8 +1,12 @@
 var container = $('.container');
 var scroller = $('.scroller');
+var social = $('.social-grid');
+
 container.hide();
 scroller.hide();
+social.hide()
 
 $(document).ready(function(){
 	container.delay(100).fadeIn(1000);
+	social.delay(200).fadeIn(1000);
 });
